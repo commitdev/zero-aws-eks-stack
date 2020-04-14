cd31bf4f05e92480d50a6931c2cb0f4a297dddff
Bill Monkman <bmonkman@gmail.com>
2020-04-13 17:21:14 -0700

    Add support for cluster autoscaler and cert manager in EKS
    Both using helm and w/support for IAM roles mapped to ServiceAccounts.
    Unfortunately still need to have a local copy of the CRDs and execute with null_resource for cert manager.

---
+++ kubernetes/terraform/modules/kubernetes/cluster_autoscaler.tf
@@ -0,0 +1,92 @@
+locals {
+  cluster_autoscaler_namespace = "kube-system"
+}
+
+
+resource "helm_release" "cluster_autoscaler" {
+  name       = "cluster-autoscaler"
+  repository = data.helm_repository.stable.metadata[0].name
+  chart      = "cluster-autoscaler"
+  namespace  = local.cluster_autoscaler_namespace
+
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = var.cluster_name
+  }
+  set {
+    name  = "rbac.create"
+    value = true
+  }
+  set_string {
+    name  = "rbac.serviceAccountAnnotations.eks\\.amazonaws\\.com/role-arn"
+    value =  module.iam_assumable_role_cluster_autoscaler.this_iam_role_arn
+  }
+  set {
+    name  = "awsRegion"
+    value = var.region
+  }
+  set {
+    name  = "extraArgs.v"
+    value = 2
+  }
+}
+
+
+# Create a role using oidc to map service accounts
+module "iam_assumable_role_cluster_autoscaler" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "~> v2.6.0"
+  create_role                   = true
+  role_name                     = "<% .Name %>-k8s-${var.environment}-cluster-autoscaler"
+  provider_url                  = replace(data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer, "https://", "")
+  role_policy_arns              = [aws_iam_policy.cluster_autoscaler.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${local.cluster_autoscaler_namespace}:cluster-autoscaler-aws-cluster-autoscaler"]
+}
+
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name_prefix = "cluster-autoscaler"
+  description = "EKS cluster-autoscaler policy for cluster ${var.cluster_name}"
+  policy      = data.aws_iam_policy_document.cluster_autoscaler_policy_doc.json
+}
+
+data "aws_iam_policy_document" "cluster_autoscaler_policy_doc" {
+  statement {
+    sid    = "eksWorkerAutoscalingAll"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "ec2:DescribeLaunchTemplateVersions",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "eksWorkerAutoscalingOwn"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${var.cluster_name}"
+      values   = ["owned"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
+      values   = ["true"]
+    }
+  }
+}
