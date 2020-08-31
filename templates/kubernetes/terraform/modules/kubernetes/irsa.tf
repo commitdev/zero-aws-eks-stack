58b3dda6e0a0da8cd77dbaa023708988ca068b0f
Steven Shi <sshi100@hotmail.com>
2020-08-31 10:15:13 -0700

    refresh naming

--- templates/kubernetes/terraform/modules/kubernetes/irsa.tf
+++ templates/kubernetes/terraform/modules/kubernetes/irsa.tf
@@ -3,14 +3,14 @@
 # application_policy_list is passed from main.tf as below:
   # application_policy_list = [
   #   {
-  #     application     = "app1"
+  #     service_account = "app1"
   #     namespace       = "piggycloud-me"
-  #     policy          = data.aws_iam_policy_document.my_app
+  #     policy          = data.aws_iam_policy_document.resource_access_app1
   #   },
   #   {
-  #     application     = "app2"
+  #     service_account = "app2"
   #     namespace       = "piggycloud-me"
-  #     policy          = data.aws_iam_policy_document.my_app
+  #     policy          = data.aws_iam_policy_document.resource_access_app2
   #   }
   #   # could be more here
   # ]
@@ -21,17 +21,17 @@ module "iam_assumable_role_irsa" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "~> v2.14.0"
   create_role                   = true
-  role_name                     = "${var.project}-k8s-${var.environment}-${var.application_policy_list[count.index].application}"
+  role_name                     = "${var.project}-k8s-${var.environment}-${var.application_policy_list[count.index].service_account}"
   provider_url                  = replace(data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer, "https://", "")
   role_policy_arns              = [aws_iam_policy.irsa[count.index].arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.application_policy_list[count.index].namespace}:${var.application_policy_list[count.index].application}"]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.application_policy_list[count.index].namespace}:${var.application_policy_list[count.index].service_account}"]
 }
 
 # Create policies
 resource "aws_iam_policy" "irsa" {
   count       = length(var.application_policy_list)
   name_prefix = var.project
-  description = "${var.project} policy for service account ${var.application_policy_list[count.index].application}"
+  description = "${var.project} policy for service account ${var.application_policy_list[count.index].service_account}"
   policy      = var.application_policy_list[count.index].policy.json
 }
 
@@ -39,7 +39,7 @@ resource "aws_iam_policy" "irsa" {
 resource "kubernetes_service_account" "irsa" {
   count         = length(var.application_policy_list)
   metadata {
-    name        = var.application_policy_list[count.index].application
+    name        = var.application_policy_list[count.index].service_account
     namespace   = var.application_policy_list[count.index].namespace
     annotations = {
       "eks.amazonaws.com/role-arn" = module.iam_assumable_role_irsa[count.index].this_iam_role_arn
