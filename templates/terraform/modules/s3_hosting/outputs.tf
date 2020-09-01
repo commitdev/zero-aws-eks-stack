2b358e222f8bcdb547202dc8eb35a6025ff99b94
Bill Monkman <bmonkman@gmail.com>
2020-08-31 17:59:16 -0700

    #75 : Remove modules that were moved to the terraform-aws-zero repo

--- templates/terraform/modules/s3_hosting/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cloudfront_distribution_ids" {
-  description = "Identifiers of the created cloudfront distributions"
-  value       = values(aws_cloudfront_distribution.client_assets_distribution)[*].id
-}
