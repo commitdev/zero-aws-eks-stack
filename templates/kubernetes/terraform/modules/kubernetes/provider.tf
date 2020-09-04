020f1012813004926d8d9e607629867a7ce0a1cd
David Cheung <davidcheung@live.ca>
2020-09-04 19:20:15 -0400

    fix: exec locally instead of aws_eks_cluster_auth (#82)
    token generated by the data.aws_eks_cluster_auth has a bug where it
    generates a token for the wrong region, leading to other users cannot
    `terraform plan` even though they have the correct permission

--- templates/kubernetes/terraform/modules/kubernetes/provider.tf
+++ templates/kubernetes/terraform/modules/kubernetes/provider.tf
@@ -14,3 +14,20 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
   load_config_file       = false
 }
+provider "kubernetes" {
+  ## This is a workaround because aws-eks-cluster-auth will default to us-east-1
+  ## leading to an invalid token to access the cluster
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args        = [
+      "eks",
+      "get-token",
+      "--region",
+      var.region,
+      "--cluster-name",
+      var.cluster_name,
+      "--role",
+      "arn:aws:iam::${data.aws_caller_identity.account_id}:role/${var.project}-kubernetes-admin-${var.environment}"]
+  }
+}
