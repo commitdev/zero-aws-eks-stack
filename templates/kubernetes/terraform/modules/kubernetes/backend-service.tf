96499b2d7686fdf4831fcc6990a122ed040bc1e4
Aaron Oman <groovestomp@groovestomp.com>
2020-09-08 15:46:21 -0700

    zero-234 Image Hosting Support
    https://app.zenhub.com/workspace/o/commitdev/zero/issues/234
    
    - Moves backend app namespace configuration to main.tf from database-service.tf.
    - Adds fileuploads question to cli interview.
    - Adds new terraform for the backend app to backend-service.tf.

---
+++ templates/kubernetes/terraform/modules/kubernetes/backend-service.tf
@@ -0,0 +1,17 @@
+data "aws_secretsmanager_secret_version" "cf_keypair" {
+  secret_id = data.aws_secretsmanager_secret.cf_keypair.id
+  value = jsondecode(data.aws_secretsmanager_secret_version.cf_keypair.secret_string)
+}
+
+resource "kubernetes_secret" "cf_keypair" {
+  metadata {
+    name = "cf_keypair"
+  }
+
+  data = {
+    keypair_id = aws_secretsmanager_secret_version.cf_keypair_secret.value["keypair_id"]
+    secret_key = aws_secretsmanager_secret_version.cf_keypair_secret.value["secret_key"]
+  }
+
+  type = "Opaque"
+}
