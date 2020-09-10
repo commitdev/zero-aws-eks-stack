bacc35239cf803135f02068aa15b87856f92c01d
Aaron Oman <groovestomp@groovestomp.com>
2020-09-10 13:41:11 -0700

    zero-234 Image Hosting Support (#86)
    * zero-234 Image Hosting Support
    
    https://app.zenhub.com/workspace/o/commitdev/zero/issues/234
    
    - Moves backend app namespace configuration to main.tf from database-service.tf.
    - Adds fileuploads question to cli interview.
    - Adds new terraform for the backend app to backend-service.tf.
    
    * Kubernetes doesn't allow underscores in resource names
    
    * Rename files to match convention
    
    * Add script for importing CF keypair data to Secrets Manager
    
    * Improves labelling of file uploads query.
    Improves comments in CF keypair import script.
    
    * zero-234: Fixup secretsmanager usage and secret naming
    
    - Specify the aws_secretsmanager_secret data source that was missing previously
    - Fix the aws_secretsmanager_secret_version data source to refer to the previous properly
    - Use a local var to store the decoded json instead of assigning it to an unavailable value attribute
    - Use 'private_key' instead of 'keypair_secret' for the private key secret key-name
    
    * zero-234: PR feedback
    
    - Fix naming of aws secretsmanager secret data source
    - Fix resource referenced by aws secretsmanager secret version data source
    - Correctly set the namespace for the kubernetes_secret resource
    - Fix the naming of the k8s secret to match the aws secretsmanager secret name
    - Use help text when too few parameters are supplies to CF keypair import script
    
    * Add k8s secret name back in
    
    * Update templated README with instructions on using import-cf-keypair.sh

---
+++ templates/scripts/import-cf-keypair.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+PROGNAME=$(basename "$0")
+
+function usage() {
+    echo "Usage: ${PROGNAME} [ -h | --help ] ID FILE"
+    echo
+    echo "Imports the specified CF Keypair data into AWS SecretsManager."
+    echo
+    echo "ID: CloudFront Keypair ID (Access Key ID)"
+    echo "FILE: CloudFront Keypair private key file"
+    echo
+    echo "See: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-trusted-signers.html#private-content-creating-cloudfront-key-pairs"
+}
+
+PARSED_ARGUMENTS=$(getopt -n ${PROGNAME} -o h --long help -- "$@")
+
+while : ; do
+    case "$1" in
+        -h | --help)
+            shift
+            usage
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ "$#" -ne 2 ]; then
+    usage
+    exit 1
+fi
+
+ID=$1
+SECRET=$(tr -d '\r' < $2 | awk '{printf "%s\\n", $0}')
+
+aws secretsmanager \
+    create-secret \
+    --name <% .Name %>_cf_keypair \
+    --region <% index .Params `region` %> \
+    --secret-string "{\"keypair_id\":\"${ID}\",\"private_key\":\"${SECRET}\"}"
