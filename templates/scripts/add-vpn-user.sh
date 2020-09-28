12af36e84e88a7fe70f10121f8a8fd650514bbcb
Steven Shi <sshi100@hotmail.com>
2020-09-28 14:19:11 -0700

    support DNS for VPN client

--- templates/scripts/add-vpn-user.sh
+++ templates/scripts/add-vpn-user.sh
@@ -4,9 +4,11 @@ CLUSTER=$(kubectl config current-context | cut -d"/" -f2)
 
 # this is a local script for a system user to generate VPN configuration for cluster ${CLUSTER}
 
+NAMESPACE=<% .Name %>
+REGION=<% index .Params `region` %>
+
 # get pod id for execution
-POD=$(kubectl -n vpn get pods | grep wireguard | cut -d' ' -f1)
-EXTERNAL_DNS=$(kubectl -nvpn get svc wireguard -o jsonpath='{.metadata.annotations.external-dns\.alpha\.kubernetes\.io/hostname}')
+POD=$(kubectl -n vpn get pods --selector=app=wireguard -o jsonpath='{.items[0].metadata.name}')
 
 if [ -z "$POD" ]; then
   echo "Warning: No VPN service running yet"
@@ -15,8 +17,7 @@ fi
 EXEC="kubectl -n vpn exec -it $POD -- /bin/bash -c"
 
 # get name
-echo -n "Enter your name: "
-read name
+echo -n "Enter your name: " && read name
 
 # collect keys
 server_public_key=$($EXEC "cat /etc/wireguard/privatekey | wg pubkey")
@@ -30,6 +31,21 @@ next_ip=$last_ip
 while [[ "$existing_ips" =~ "$next_ip" ]]; do
   next_ip=${next_ip%.*}.$((${next_ip##*.}+1))
 done
+
+# get DNS server setting
+dns_server=$($EXEC "cat /etc/resolv.conf | grep nameserver | tail -1 | cut -d\" \" -f2 | tr -d \"\r\n\f\"")
+
+# get VPC CIDR for allowed IP subnet
+VPCNAME=${CLUSTER%-$REGION}-vpc
+vpc_cidr=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=${VPCNAME} | jq -r '.Vpcs[].CidrBlock')
+[[ -z "$vpc_cidr" ]] && vpc_cidr = "10.10.0.0/16"
+
+# get DB server
+K8S_DBSERVER=database.${NAMESPACE}.svc.cluster.local
+AWS_DBSERVER=$(kubectl -n piggycloud2-me get svc -ojsonpath='{.items[0].spec.externalName}')
+
+# get Endpoint DNS
+EXTERNAL_DNS=$(kubectl -nvpn get svc wireguard -o jsonpath='{.metadata.annotations.external-dns\.alpha\.kubernetes\.io/hostname}')
 
 # generate config file
 CONFIG_DIR=~/.wireguard
@@ -50,7 +66,11 @@ echo
 echo "After this is done you should be able to open the wireguard client and activate the tunnel."
 echo "You can download the client at https://www.wireguard.com/install/"
 echo
-echo "When it is running you should be able to access internal resources, e.g. mysql -h 10.10.10.123"
+echo "When it is running you should be able to access internal resources, eg. database server:" 
+echo "  with Kubernetes cluster DNS, run:"
+echo "      mysql -u <user name> -p -h ${K8S_DBSERVER}"
+echo "  with AWS RDS, run:"
+echo "      mysql -u <user name> -p -h ${AWS_DBSERVER}"
 # generate client conf
 cat <<-EOF > ${CONFIG_FILE}
 
@@ -65,11 +85,12 @@ cat <<-EOF > ${CONFIG_FILE}
 PrivateKey = $client_private_key
 ListenPort = 34567
 Address = $next_ip/32
+DNS = $dns_server
 
 [Peer]
 # VPN server side
 PublicKey = $server_public_key
-AllowedIPs = 10.10.0.0/16
+AllowedIPs = $vpc_cidr, $dns_server/32
 Endpoint = $EXTERNAL_DNS:51820
 
 EOF
