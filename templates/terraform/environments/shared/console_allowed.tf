73b52612fe284a00a379512d912534a5b1bc673d
Steven Shi <sshi100@hotmail.com>
2020-09-29 22:24:52 -0700

    allow console password to to be accessible...

--- templates/terraform/environments/shared/console_allowed.tf
+++ templates/terraform/environments/shared/console_allowed.tf
@@ -1,6 +1,14 @@
 # AWS policy documents for allowing console
 data "aws_iam_policy_document" "console_allowed" {
   statement {
+    sid       = "AllowConsolePasswordDisplay"
+    effect    = "Allow"
+    actions = [
+      "iam:ListUsers",
+    ]
+    resources = ["arn:aws:iam::${local.account_id}:user/*"]
+  }
+  statement {
     sid     = "AllowManageOwnProfile"
     effect  = "Allow"
     actions = [
