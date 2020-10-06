8153de714c7fbacdcbaede8789344341ecb6194e
Steven Shi <sshi100@hotmail.com>
2020-10-06 12:11:44 -0700

    use user_access with version 0.1.8

--- templates/terraform/modules/environment/main.tf
+++ templates/terraform/modules/environment/main.tf
@@ -154,7 +154,7 @@ module "sendgrid" {
 
 module "user_access" {
   source  = "commitdev/zero/aws//modules/user_access"
-  version = "0.1.3"
+  version = "0.1.8"
 
   project     = var.project
   environment = var.environment
