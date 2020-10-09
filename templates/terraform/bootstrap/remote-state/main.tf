fa5a9028b9ddb5fe39fbc58e5d35e1d95fc10d55
Steven Shi <sshi100@hotmail.com>
2020-10-09 14:06:50 -0700

    add S3 encryption

--- templates/terraform/bootstrap/remote-state/main.tf
+++ templates/terraform/bootstrap/remote-state/main.tf
@@ -10,6 +10,14 @@ resource "aws_s3_bucket" "terraform_remote_state" {
   versioning {
     enabled = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_remote_state" {
