baf4a9006e09f46f5b9546909e7bf88700d5cda5
Bill Monkman <bmonkman@gmail.com>
2020-11-25 10:10:27 -0800

    Maintenance and fixes (#163)
    * Initial maintenance and fixes
    
    Bump version of nginx ingress controller, cluster autoscaler, cert manager
    Change helm repo of cluster autoscaler, cert manager
    Move db-ops file into templates/scripts/ because we need to hook into the actual steps of the apply
    
    * Moved execution of db scripts into templated makefile, some small fixes
    
    * Fixes to annotations and config for upgraded charts
    
    * Update metrics-server to new chart

--- templates/scripts/post-apply.sh
+++ templates/scripts/post-apply.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
-set -e
+
+KUBE_CONTEXT=${PROJECT}-${ENVIRONMENT}-${AWS_DEFAULT_REGION}
+RANDOM_SEED="<% index .Params `randomSeed` %>"
 
 <% if ne (index .Params `loggingType`) "kibana" %># <% end %>source elasticsearch-logging.sh
+
+kubectl --context ${KUBE_CONTEXT} -n ${PROJECT} get secrets ${PROJECT} > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    REGION=${AWS_DEFAULT_REGION} \
+    SEED=${RANDOM_SEED} \
+    PROJECT_NAME=${PROJECT} \
+    ENVIRONMENT=${ENVIRONMENT} \
+    NAMESPACE=${PROJECT} \
+    DATABASE_TYPE=<% index .Params `database` %> \
+    DATABASE_NAME=${PROJECT} \
+    USER_NAME=${PROJECT} \
+    CREATE_SECRET=secret-application.yml.tpl \
+    sh ./create-db-user.sh
+fi
