c787af1fd3f3671f7614a2cdad658a1b96aee20b
Bill Monkman <bmonkman@gmail.com>
2020-11-24 16:08:13 -0800

    Update metrics-server to new chart

--- templates/kubernetes/terraform/modules/kubernetes/metrics_server.tf
+++ templates/kubernetes/terraform/modules/kubernetes/metrics_server.tf
@@ -5,17 +5,16 @@ locals {
 
 resource "helm_release" "metrics_server" {
   name       = "metrics-server"
-  repository = "https://kubernetes-charts.storage.googleapis.com"
+  # @TODO : The central chart repo was deprecated but as of late 2020 this chart didn't have an official home.
+  # Switch away from this bitnami one when possible. https://github.com/kubernetes-sigs/metrics-server/issues/572
+  repository = "https://charts.bitnami.com/bitnami"
   chart      = "metrics-server"
+  version    = "5.0.2"
   namespace  = local.metrics_server_namespace
 
   set {
-    name  = "args"
-    value = "{--kubelet-preferred-address-types=InternalIP}"
-  }
-  set {
-    name  = "image.tag"
-    value = "v0.3.6"
+    name  = "apiService.create"
+    value = true
   }
 }
 
