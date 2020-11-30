5408db6a5ea2711acfc40941764268952b0583be
Steven Shi <sshi100@hotmail.com>
2020-11-30 15:56:49 -0800

    enhancement with jq instead of sed

--- templates/scripts/create-dev-env.sh
+++ templates/scripts/create-dev-env.sh
@@ -5,7 +5,7 @@ AWS_DEFAULT_REGION=<% index .Params `region` %>
 RANDOM_SEED="<% index .Params `randomSeed` %>"
 ENVIRONMENT=stage  # only apply to Staging environment
 
-DEV_DB_LIST=$(aws iam get-group --group-name ${PROJECT}-developer-${ENVIRONMENT} | jq -r .Users[].UserName | sed 's/^/dev/' | tr '\n' ' ')
+DEV_DB_LIST=$(aws iam get-group --group-name ${PROJECT}-developer-${ENVIRONMENT} | jq -r '"dev" + .Users[].UserName' | tr '\n' ' ')
 if [[ -z "${DEV_DB_LIST}" ]]; then
     echo "$0: No developers available"
     exit 2
