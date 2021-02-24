c152354cb656a0e698a0ffd8f15f1b6e279e521d
David Cheung <davidcheung@live.ca>
2021-02-24 14:30:23 -0500

    fixup! move auth into module for better portability

--- templates/kubernetes/terraform/modules/kubernetes/user_auth.tf
+++ templates/kubernetes/terraform/modules/kubernetes/user_auth.tf
@@ -8,15 +8,15 @@ locals {
 }
 
 module "user_auth" {
-  for_each = { for index, auth_instance in var.user_auth : index => auth_instance }
+  count = length(var.user_auth)
   source                      = "./user_auth"
 
-  project = each.value.name
-  auth_domain                 = each.value.auth_domain
-  auth_namespace              = each.value.auth_namespace
-  frontend_service_domain     = each.value.frontend_service_domain
-  backend_service_domain      = each.value.backend_service_domain
-  user_auth_mail_from_address = each.value.user_auth_mail_from_address
-  jwks_secret_name            = each.value.jwks_secret_name
+  project                     = var.user_auth[count.index].name
+  auth_domain                 = var.user_auth[count.index].auth_domain
+  auth_namespace              = var.user_auth[count.index].auth_namespace
+  frontend_service_domain     = var.user_auth[count.index].frontend_service_domain
+  backend_service_domain      = var.user_auth[count.index].backend_service_domain
+  user_auth_mail_from_address = var.user_auth[count.index].user_auth_mail_from_address
+  jwks_secret_name            = var.user_auth[count.index].jwks_secret_name
   k8s_local_exec_context      = local.k8s_exec_context
 }
