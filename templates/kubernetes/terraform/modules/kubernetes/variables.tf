dc60ff65ab356ebdb6dc4f8cb72caa60ae6175a0
David Cheung <davidcheung@live.ca>
2021-03-05 17:10:42 -0500

    fixup! fixup! fixup! fixup! fixup! fixup! add cookie signing secret key

--- templates/kubernetes/terraform/modules/kubernetes/variables.tf
+++ templates/kubernetes/terraform/modules/kubernetes/variables.tf
@@ -130,5 +130,6 @@ variable "user_auth" {
     jwks_secret_name            = string
     user_auth_mail_from_address = string
     whitelisted_return_urls     = list(string)
+    cookie_sigining_secret_key  = string
   }))
 }
