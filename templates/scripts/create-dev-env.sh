8a89c3a3866b7c72c00773a8a62cd92aa2fd0b90
David Cheung <davidcheung@live.ca>
2021-09-03 17:21:49 -0400

    dev env: fix secret creation for external secret
    also defaults VPN IP to 201 when none exist

--- templates/scripts/create-dev-env.sh
+++ templates/scripts/create-dev-env.sh
@@ -3,9 +3,12 @@
 PROJECT=<% .Name %>
 AWS_DEFAULT_REGION=<% index .Params `region` %>
 RANDOM_SEED="<% index .Params `randomSeed` %>"
+DATABASE_TYPE=<% index .Params `database` %>
 ENVIRONMENT=stage  # only apply to Staging environment
 
-DEV_DB_LIST=$(aws iam get-group --group-name ${PROJECT}-developer-${ENVIRONMENT} | jq -r '"dev" + .Users[].UserName' | tr '\n' ' ')
+## Creating each member in developer IAM group a dev-database
+## name: john-doe -> dbname: dev_johndoe
+DEV_DB_LIST=$(aws iam get-group --group-name ${PROJECT}-developer-${ENVIRONMENT} | jq -r '"dev_" + .Users[].UserName' | tr '\n' ' ')
 if [[ -z "${DEV_DB_LIST}" ]]; then
     echo "$0: No developers available yet, skip."
     exit 0
@@ -20,11 +23,11 @@ if [[ $? -eq 0 ]]; then
     PROJECT_NAME=${PROJECT} \
     ENVIRONMENT=${ENVIRONMENT} \
     NAMESPACE=${PROJECT} \
-    DATABASE_TYPE=<% index .Params `database` %> \
+    DATABASE_TYPE=${DATABASE_TYPE} \
     DATABASE_NAME="${DEV_DB_LIST}" \
-    SECRET_NAME=devenv${PROJECT} \
+    SECRET_NAME=devenv-${PROJECT} \
     USER_NAME=dev${PROJECT} \
     USER_PASSWORD=${DEV_DB_SECRET} \
-    CREATE_SECRET=secret-application.yml.tpl \
+    CREATE_SECRET=secret-application.json.tpl \
     sh ./create-db-user.sh
 fi
